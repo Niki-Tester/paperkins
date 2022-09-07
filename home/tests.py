d385d670c937ef05bea696161f5840abb2bb4cde
Niki Tester <bloatedniknak@gmail.com>
2022-09-07 19:31:12 +0000

    add test for home page

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHomeApp(TestCase):
+    """
+    Test home app
+    """
+
+    def test_home_page(self):
+        "Test home page renders correct page"
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
