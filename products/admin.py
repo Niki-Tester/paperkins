8ac12a815ea499c6621eed550bb8a82214c81af0
Niki Tester <bloatedniknak@gmail.com>
2022-09-09 06:58:58 +0000

    feat: Add admin action active/inactive

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from .models import Product, Category
 
 
+@admin.action(description='Mark selected as active')
+def make_active(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+
+@admin.action(description='Mark selected as inactive')
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(active=False)
+
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'stock_qty', 'rating', 'active')
     ordering = ('name',)
+    actions = [make_active, make_inactive]
 
 
 class CategoryAdmin(admin.ModelAdmin):
