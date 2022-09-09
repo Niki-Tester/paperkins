8bcfec3a3d6ee7ef3938c4e128fdb020e66982bf
Niki Tester <bloatedniknak@gmail.com>
2022-09-09 06:10:22 +0000

    feat: prevent editing of product rating

--- products/models.py
+++ products/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+        max_digits=6, decimal_places=2, null=True, blank=True, editable=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     stock_qty = models.IntegerField(default=0)
