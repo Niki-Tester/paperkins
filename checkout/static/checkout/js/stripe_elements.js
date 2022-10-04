824635fa0e644ac8687dbd59517f2f9a95966a70
Niki Tester <bloatedniknak@gmail.com>
2022-10-04 21:10:08 +0000

    feat: add billing/shipping details to stripe payment

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -54,7 +54,32 @@ form.addEventListener('submit', e => {
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
-        }
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+                address: {
+                    line1: $.trim(form.street_address_1.value),
+                    line2: $.trim(form.street_address_2.value),
+                    city: $.trim(form.town_or_city.value),
+                    state: $.trim(form.county.value),
+                    postal_code: $.trim(form.postcode.value),
+                    country: $.trim(form.country.value),
+                }
+            }
+        },
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address_1.value),
+                line2: $.trim(form.street_address_2.value),
+                city: $.trim(form.town_or_city.value),
+                state: $.trim(form.county.value),
+                postal_code: $.trim(form.postcode.value),
+                country: $.trim(form.country.value),
+            }
+        },
     }).then(result => {
         const errorDiv = document.getElementById('card-errors')
         if (result.error) {
