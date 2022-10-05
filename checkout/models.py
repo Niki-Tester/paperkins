1871063e4fdf28f825451210ca4215223cea3e98
Niki Tester <bloatedniknak@gmail.com>
2022-10-05 20:39:22 +0000

    feat: add order history and default info functionality

--- checkout/models.py
+++ checkout/models.py
@@ -10,7 +10,7 @@ from profiles.models import UserProfile
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
 
-    user = models.ForeignKey(
+    user_profile = models.ForeignKey(
         UserProfile,
         on_delete=models.SET_NULL,
         null=True,
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from .models import UserProfile
+from checkout.models import Order
 from .forms import UserProfileForm
 
 
 def profile(request):
     """ A view for rendering user profiles """
     profile = get_object_or_404(UserProfile, user=request.user)
-    print(profile.__dict__)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
@@ -29,3 +29,19 @@ def profile(request):
         'profile': profile
     }
     return render(request, 'profiles/profile.html', context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request,
+                  'This is a past confirmation for '
+                  f'order number: {order.order_number}. '
+                  f'A confirmation email was sent to: {order.email} '
+                  f'on {order.date}')
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order
+    }
+
+    return render(request, template, context)
