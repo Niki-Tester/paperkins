2a967322e2d0694fb9dbca802bb56884ac08f0d1
Niki Tester <bloatedniknak@gmail.com>
2022-10-05 10:32:45 +0000

    add userprofile models and template html

--- checkout/models.py
+++ checkout/models.py
@@ -4,10 +4,19 @@ from django.db.models import Sum
 from products.models import Product
 from django.conf import settings
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+
+    user = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders')
+
     date = models.DateTimeField(auto_now=True)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -17,17 +26,39 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=90, null=False, blank=False)
     county = models.CharField(max_length=90, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
+
     country = CountryField(
-        blank_label='Country *', null=False, blank=False)
+        blank_label='Country *',
+        null=False,
+        blank=False)
+
     delivery_cost = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        default=0)
+
     order_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, blank=False, default=0)
+        max_digits=10,
+        decimal_places=2,
+        null=False, blank=False,
+        default=0)
+
     grand_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, blank=False, default=0)
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        default=0)
+
     original_cart = models.TextField(null=False, blank=False, default='')
+
     stripe_pid = models.CharField(
-        max_length=254, null=False, blank=False, default='')
+        max_length=254,
+        null=False,
+        blank=False,
+        default='')
 
     def _generate_order_number(self):
         """ Generate a random & unique order number using UUID """
--- profiles/views.py
+++ profiles/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def profile(request):
     """ A view for rendering user profiles """
-    return render(request, 'profiles/profile.html')
+    context = {}
+    return render(request, 'profiles/profile.html', context)
