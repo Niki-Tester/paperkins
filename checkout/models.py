74c18dec866f565b91e46c706c6173e17a14921a
Niki Tester <bloatedniknak@gmail.com>
2022-10-05 00:58:36 +0000

    feat: add countryfield to checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models import Sum
 from products.models import Product
 from django.conf import settings
+from django_countries.fields import CountryField
 
 
 class Order(models.Model):
@@ -16,7 +17,8 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=90, null=False, blank=False)
     county = models.CharField(max_length=90, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(
+        blank_label='Country *', null=False, blank=False)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, blank=False, default=0)
     order_total = models.DecimalField(
