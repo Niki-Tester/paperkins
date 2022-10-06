ece9d41c1dfdd86c5e605f0e1dbe5a9dc6d63f57
Niki Tester <bloatedniknak@gmail.com>
2022-10-06 12:45:31 +0000

    fix: temp enable debug to debug deleting products on production

--- paperkins/settings.py
+++ paperkins/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT_ENV' in os.environ
+# DEBUG = 'DEVELOPMENT_ENV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['paperkins.herokuapp.com', 'localhost']
 
