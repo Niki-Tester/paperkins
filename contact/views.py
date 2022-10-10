2777353f159b0b480b27551b441606a6079d206b
Niki Tester <bloatedniknak@gmail.com>
2022-10-10 15:13:07 +0000

    fix: remove unsed import

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse, redirect
+from django.shortcuts import redirect
 from django.contrib import messages
 from django.views.decorators.http import require_POST
 from .forms import ContactForm
