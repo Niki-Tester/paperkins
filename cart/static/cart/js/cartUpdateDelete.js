83228d50c3ebee786605e1f451ef306ee1cf5080
Niki Tester <bloatedniknak@gmail.com>
2022-10-13 22:43:57 +0000

    feat: add boostrap init js file

---
+++ cart/static/cart/js/cartUpdateDelete.js
@@ -0,0 +1,31 @@
+// Update Quantity on Click
+$('.update-link').click(e => {
+    e.preventDefault()
+    const form = $(e.currentTarget).prev('.update-form');
+    form.submit();
+})
+
+// Remove item and reload on Click
+$('.remove-link').click(e => {
+    e.preventDefault()
+    const csrftoken = "{{ csrf_token }}"
+    const itemId = $(e.currentTarget).attr('id').split('remove_')[1];
+    const customMessage = $(e.currentTarget).data('customMessage');
+    const url = `remove/${itemId}`
+    let data = {}
+    if (customMessage || customMessage == '') {
+        data = {
+            'csrfmiddlewaretoken': csrftoken,
+            'custom_message': customMessage,
+        }
+    } else {
+        data = {
+            'csrfmiddlewaretoken': csrftoken,
+        }
+    }
+
+    $.post(url, data)
+        .done(() => {
+            location.reload()
+        })
+})
